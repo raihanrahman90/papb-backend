d8ac0d719944375e2950ebbb8f231688e950e289
Muhammad Raihan Rahman <11181061@student.itk.ac.id>
2021-09-12 16:44:46 +0800

    add swagger

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,21 @@ app.use(
         extended:true
     })
 )
+const swaggerOptions = {
+    swaggerDefinition:{
+        info: {
+            title: 'PAPB API',
+            desctiption: "PAPB API Information",
+            contact:{
+                name: "Raihan Rahman"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    apis:["./routes/*.js"]
+}
+
+const swaggerDocs = require('./swagger.json');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,5 +39,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/barang', barangRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = app;
--- routes/barangRoute.js
+++ routes/barangRoute.js
@@ -3,8 +3,37 @@ var express = require('express');
 var router = express.Router();
 const barangController = require('../controller/barang');
 
-/* GET users listing. */
+// Routes
 router.route('/')
+/**
+ * @swagger
+ * /barang:
+ *  get:
+ *      description: Use to get all barang
+ *      responses:
+ *          '200':
+ *              description: A successfull response
+ *  post:
+ *      description: Use to insert new barang
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          namaBarang:
+ *                              type: string
+ *                          hargaBarang:
+ *                              type: integer
+ *                      required:
+ *                          - namabarang
+ *                          - hargaBarang  
+ *      responses:
+ *          '200':
+ *              description: A Successfull response
+ * 
+ */
 .get(barangController.getAll)
 .post(barangController.create)
 
